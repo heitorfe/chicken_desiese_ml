4270e45bdf97bc97dc090d269d704969c0615f56
heitorfe <heitorfelix@yahoo.com.br>
2023-11-23 06:10:42 -0300

    fix main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ STAGE_NAME = "Data Ingestion Stage"
 try: 
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main
+    data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
 except Exception as e:
