31c7b7a394333ef26d552a789818c8dd65eb5783
heitorfe <heitorfelix@yahoo.com.br>
2023-11-23 06:10:58 -0300

    fix main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion  import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import  PrepareBaseModelPipeline
 from src.cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline  
-from src.cnnClassifier.pipeline.stage_04_evaluation import EvolutionPipeline
+from src.cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
